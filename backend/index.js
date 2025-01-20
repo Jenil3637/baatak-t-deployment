20c44d5a5472bfe2c7fe14d4bf8d0773036b80b9
Jenil Patel <jenilpatelh9@gmail.com>
2025-01-20 22:07:48 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -22,9 +22,7 @@ app.use(urlencoded({ extended: true }));
 // CORS Configuration
 const corsOptions = {
     origin: [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "https://baattak.vercel.app"
+        "https://baattak.onrender.com"
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true, // Allow sending cookies and other credentials
